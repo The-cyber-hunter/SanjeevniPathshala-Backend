540e0a9d6e78efb1bc5edf0714d702f38a3258ec
The_cyber_hunter <singhansh6173@gmail.com>
2025-12-08 00:03:41 +0530

    removed unnecassary code

--- server.js
+++ server.js
@@ -31,21 +31,6 @@ app.post(
 );
 
 app.use(express.json());
-(async () => {
-  try {
-    const admin = await Admin.findOne();
-    if (!admin) {
-      await Admin.create({
-        email: "admin@sanjeevni.com",
-        password: await bcrypt.hash("admin123", 10),
-      });
-
-      console.log("Default Admin Created → Email: admin@sanjeevni.com Password: admin123");
-    }
-  } catch (error) {
-    console.log("Error seeding admin:", error.message);
-  }
-})();
 
 // -----------------------
 // ROUTES
@@ -56,6 +41,5 @@ app.use("/api/student", studentRoute);
 app.use("/api/contact", contactRoute);
 app.use("/api/admin", authRoute);   
 
-// -----------------------
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
